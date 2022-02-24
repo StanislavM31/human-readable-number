11fce1c1b560cdf78e4d530f06b6707954dc4a82
Stanislav Minko <minkostas@gmail.com>
2022-02-25 01:45:19 +0300

    feat: processing 99

--- src/index.js
+++ src/index.js
@@ -1,16 +1,61 @@
 module.exports = function toReadable (number) {
+    let mas = [
+        'zero',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'hundred'
+    ]
 
-        if(typeof(number) == "undefined") return;
-       if(number % 100 >= 11 && number % 100 <= 13)
-             return number + "th";
-             
-             switch(number % 10) {
-                 case 1: return number + "st";
-                 case 2: return number + "nd";
-                 case 3: return number + "rd";
-             }
-             return number + "th";
-         
+    const teen = [
+        "null",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const twenty_ninety = [
+        "null",
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    const numStr = number.toString();
+    const numLeng = numStr.length;
+
+    if (number < 10) {
+        return mas[number];
+    }
+
+    if (numLeng === 2) {
+        if (10 < number && number < 20) {
+            return teen[numStr[1]];
+        } else if (9 < number && number < 91 && numStr[1] === "0") {
+            return twenty_ninety[numStr[0]];
+        }
+        return `${twenty_ninety[numStr[0]]} ${mas[numStr[1]]}`;
+    } else if (numLeng === 3) {
+        return `${mas[numStr[0]]} hundred`;
+    }
 }
+
+
 
  
